a4a799ea024a7f100426eb0ba9e812b45ba5c3d5
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-10-02 20:30:45 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,84 +1,63 @@
 #!/usr/bin/python3
-# square.py
-"""Defines a square class."""
+
+"""A module for Square class"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """A Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
-        """
+        """Initialize square class"""
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Get/set the size of the Square."""
-        return self.width
+        """To retrieve size"""
+        return self.__width
 
     @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        """Update the Square.
-
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
-
-    def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
+    def size(self, size):
+        """To set size"""
+        if type(size) != int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = size
+        self.__height = size
 
     def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
+        """Creates a string object from a given object"""
+        end_string = "[Square] "
+        end_string += "({}) ".format(self.id)
+        end_string += "{:d}/{:d} - ".format(self.x, self.y)
+        end_string += "{:d}".format(self.size)
+        return end_string
+
+    def update(self, *args, **kwargs):
+        """Assigns a key/value argument to each attribute"""
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
+
+        if "id" in kwargs:
+            self.id = kwargs["id"]
+
+        if "size" in kwargs:
+            self.size = kwargs["size"]
+
+        if "x" in kwargs:
+            self.x = kwargs["x"]
+
+        if "y" in kwargs:
+            self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """Creates string object from a given object"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
