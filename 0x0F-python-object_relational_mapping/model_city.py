8924d298b3c78c3805032d089eddde36fc361c0c
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-12-03 22:28:01 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,6 +3,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base, State
+
+
 class City(Base):
     """City class inherits from State class
 
@@ -17,7 +19,5 @@ class City(Base):
     id = Column('id', Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
     state = relationship("State", back_populates="cities")
-
 State.cities = relationship("City", order_by=City.id, back_populates="state")
