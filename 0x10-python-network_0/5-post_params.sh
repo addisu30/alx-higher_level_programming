4f907c39e77d07918f47c8d6b37569def91adfdf
addisu30 <dabaleaddisu21@outlook.com>
2022-12-16 23:51:26 +0300

    updating

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 curl -sd "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
->>>>>>> c595481877498afaebd67d361b18c1a603e0e2cc
