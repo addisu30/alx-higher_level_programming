19cda260766be42cab5218903b3d5b203009c628
addisu30 <dabaleaddisu21@outlook.com>
2023-01-18 09:46:39 +0300

    updating

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,5 @@
-t request = require('request');
+#!/usr/bin/node
+const request = require('request');
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
