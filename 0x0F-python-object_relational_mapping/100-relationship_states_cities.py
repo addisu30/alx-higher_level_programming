20c8d2202932a5d2bb4eb08a80dcbcba086991f3
addisu30 <dabaleaddisu21@outlook.com>
2023-04-01 22:02:43 +0300

    relation ship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,8 +2,8 @@
 """
 Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
 Usage: ./101-relationship_states_cities_list.py <mysql username> /
-                                                 <mysql password> /
-                                                 <database name>
+                                                <mysql password> /
+                                                <database name>
 """
 import sys
 from sqlalchemy import create_engine
@@ -15,10 +15,9 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
